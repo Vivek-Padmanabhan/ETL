7ab44087aa0e89e3763bc27875dffb0ca1678783
Vivek Padmanabhan <vivek@localoye.com>
2018-12-13 21:30:14 +0530

    Added Comments

--- innersense/models.py
+++ innersense/models.py
@@ -9,7 +9,10 @@ from django.db import models
 ################################################################################
 
 class Customer(models.Model):
-    ''''''
+    '''
+        Schema to store Customer Data.
+        Each Customer is uniquely identified by mobile field.
+    '''
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=500)
     mobile = models.BigIntegerField()
@@ -19,20 +22,29 @@ class Customer(models.Model):
     pincode = models.BigIntegerField()
 
 class Product(models.Model):
-    ''''''
+    '''
+        Schema to store Product Data.
+        Each Product is uniquely identified by a combination of sku, size and color.
+    '''
     id = models.AutoField(primary_key=True)
     sku = models.CharField(max_length=100)
     size = models.CharField(max_length=100)
     color = models.CharField(max_length=100, null=True)
 
 class Package(models.Model):
-    ''''''
+    '''
+        Schema to store Package Data.
+        Each Package can contain one or more Products.
+    '''
     id = models.AutoField(primary_key=True)
     sku = models.CharField(max_length=100)
     products = models.ManyToManyField(Product)
 
 class Orders(models.Model):
-    ''''''
+    '''
+        Schema to store Order Data.
+        Each Order corresponds to a single Package.
+    '''
     id = models.AutoField(primary_key=True)
     order_id = models.CharField(max_length=100)
     sub_order_id = models.CharField(max_length=100)
@@ -42,7 +54,10 @@ class Orders(models.Model):
     order_date = models.DateField()
 
 class Invoice(models.Model):
-    ''''''
+    '''
+        Schema to store Invoice Data.
+        Each Invoice corresponds to a single Order.
+    '''
     id = models.AutoField(primary_key=True)
     invoice_id = models.CharField(max_length=100)
     order = models.ForeignKey(Orders, blank=False, null=True, on_delete=models.SET_NULL)
